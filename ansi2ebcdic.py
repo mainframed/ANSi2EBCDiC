168e9cb83d247c861e08f8fab9b11d04c833960e
Philip Young <mainframed767@gmail.com>
2025-05-06 14:45:41 -0700

    fixed newline issue

--- ansi2ebcdic.py
+++ ansi2ebcdic.py
@@ -387,7 +387,8 @@ EGMSGLN EQU *-EGMSG
          BE    SPECLMSG                                                 23164803
          CLI   MSGINDEX,X'02'                                           23164804
          BNE   EGSKIP                                                   23164805
-SPECLMSG DS    0H                                                       23164806         LA    R3,EGMSGLN                                               23164808
+SPECLMSG DS    0H                                                       23164806
+         LA    R3,EGMSGLN                                               23164808
          L     R4,=A(EGMSG)                                             23164810
 *                                                                       23164812
          WRITE RPL=(PTRRPL),                                           X23164814
